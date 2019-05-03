66ac3740fcd89b48233540bbe2f71a1acb16f3ad
rupertwaldron <rupert.waldron@yahoo.co.uk>
2019-05-03 14:41:37 +0100

    something reset head

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a change");
+        System.out.println("This is a change we don't want");
     }
 }
